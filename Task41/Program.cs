eee43d413205f6d3c75d71a72f67a05c06d4130e
Eva <okamenoame@gmail.com>
2023-04-13 15:05:32 +0300

    добавлены изменени в решение задач

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.Write("Введите элементы массива(через запятую): ");
 int[] arr = Array.ConvertAll(Console.ReadLine()!.Split(","), int.Parse);
 int count = 0;
+PrintArray(arr);
  
 for (int i = 0; i < arr.Length; i++)
 {
@@ -10,4 +11,14 @@ for (int i = 0; i < arr.Length; i++)
     }
 }
  
-Console.WriteLine($"Кол-во элементов > 0: {count}");
+Console.WriteLine($"Кол-во элементов больше нуля: {count}");
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ",");
+    }
+    Console.Write("]");
+}
